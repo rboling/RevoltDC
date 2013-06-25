e3f79e4fec8db49af105b1153339919494ea1283
kevnsn <kevnsn@gmail.com>
2013-06-24 21:51:17 -0400

    Added custom js, jquery placeholder code

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,4 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(document).ready(function(){$("#body_fade").fadeIn(1500);});
+// You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
